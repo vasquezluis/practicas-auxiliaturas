4f36b23ff8b53d743ff871aa6a215bfeb33fa06a
vasquezluis <luivasquez95@gmail.com>
2023-06-01 00:01:41 -0600

    comments added

--- 08_ejemplo_testUnitario/src/App.jsx
+++ 08_ejemplo_testUnitario/src/App.jsx
@@ -3,6 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// #### -> En este ejemplo no se usa codigo de React, solo se usa para probar el test unitario
+// ### -> se llama test unitario porque se prueban pequeñas partes del codigo
+// ### -> se llama test de integracion cuando se prueban varias partes del codigo
+// ### -> se llama test de aceptacion cuando se prueba todo el codigo
+// ### -> se llama test de humo cuando se prueba que el codigo se ejecute
+// ### -> se llama test de regresion cuando se prueba que el codigo no se rompa
+// ### -> se llama test de carga cuando se prueba que el codigo soporte la carga
+
+// ### -> podemos usar la propiedad data-testid en algunos componentes para identificar un elemento en el test
+
+// -> este es el codigo que crea vite por defecto
 function App() {
   const [count, setCount] = useState(0)
 
--- 08_ejemplo_testUnitario/testing/tests/ejemplo.spec.js
+++ 08_ejemplo_testUnitario/testing/tests/ejemplo.spec.js
@@ -1,27 +1,41 @@
+// se importa el test y expect de playwright para hacer las pruebas
 import { test, expect } from "@playwright/test";
 
+// codigo para limpiar la consola antes de cada prueba e ir a la pagina para las pruebas
 test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:5173");
 });
 
+// se describe el grupo de pruebas
 test.describe("Pruebas de la pagina", () => {
+
+  // se describe la prueba
+  // la primera prueba indica que la pagina debe tener el titulo patitoLoco
   test("should show patitoLoco as the page title", async ({ page }) => {
     await expect(page).toHaveTitle("patitoLoco");
   });
 
+  // la segunda prueba indica que la pagina debe tener el titulo Vite + React
+  // se usa el metodo getByText para buscar el texto en la pagina
+  // se usa el metodo toBeVisible para verificar que el elemento sea visible
   test("should show a title -> Vite + React", async ({ page }) => {
     const titleInPage = await page.getByText("Vite + React");
 
     await expect(titleInPage).toBeVisible();
   });
 
+  // la tercera prueba indica que la pagina debe tener el logo de React
+  // se usa el metodo getByTestId para buscar el elemento por el atributo data-testid
+  // se usa el metodo toBeVisible para verificar que el elemento sea visible
   test("should show the React logo", async ({ page }) => {
     const img = await page.getByTestId("react-logo");
 
     await expect(img).toBeVisible();
   });
 
-  // test if the button is visible by selector
+  // la cuarta prueba indica que la pagina debe tener un boton
+  // se usa el metodo getByRole para buscar el elemento por el atributo role
+  // se usa el metodo toBeVisible para verificar que el elemento sea visible
   test("should show the button", async ({ page }) => {
     const button = await page.getByRole("button");
 

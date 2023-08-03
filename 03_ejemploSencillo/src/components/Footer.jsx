9c531e898b6217dac3395341bb9fda478d1468b7
vasquezluis <luivasquez95@gmail.com>
2023-08-02 23:27:47 -0600

    Estilos añadidos para footer

---
+++ 03_ejemploSencillo/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer () {
+  return (
+    <div className='bg-slate-900 w-full p-2 m-4 flex rounded'>
+      <h1 className='text-white'>Footer</h1>
+    </div>
+  )
+}
+
+export default Footer
